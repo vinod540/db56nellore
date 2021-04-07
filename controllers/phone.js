d101ac0c6cf760adb2835808999969d7b6815c6f
vinod540 <77802816+vinod540@users.noreply.github.com>
2021-04-07 10:30:47 -0500

    post request in postman

--- controllers/phone.js
+++ controllers/phone.js
@@ -16,10 +16,27 @@ exports.phone_list = async function(req, res) {
 exports.phone_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Phone detail: ' + req.params.id);
 };
-// Handle Phones create on POST.
-exports.phone_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Phone create POST');
+
+// Handle Costume create on POST.
+exports.phone_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Phone();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costumetype":"goat", "cost":12, "size":"large"}
+    document.name = req.body.name;
+    document.cost = req.body.cost;
+    document.brand = req.body.brand;
+    try {
+        let result = await document.save();
+        res.send(result);
+    } catch (err) {
+        res.error(500, `{"error": ${err}}`);
+    }
 };
+
+
 // Handle Phones delete form on DELETE.
 exports.phone_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Phone delete DELETE ' + req.params.id);
@@ -27,4 +44,15 @@ res.send('NOT IMPLEMENTED: Phone delete DELETE ' + req.params.id);
 // Handle Phones update form on PUT.
 exports.phone_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: Phone update PUT' + req.params.id);
+};
+
+// VIEWS
+// Handle a show all view
+exports.phone_view_all_Page = async function(req, res) {
+    try {
+        thePhones = await Phone.find();
+        res.render('phone', { title: 'Phones Search Results', results: thePhones });
+    } catch (err) {
+        res.error(500, `{"error": ${err}}`);
+    }
 };
--- routes/phone.js
+++ routes/phone.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const phone_controllers = require('../controllers/phone');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('phone', { title: 'Search Results phone' });
-});
+router.get('/', phone_controllers.phone_view_all_Page);
 
 module.exports = router;
